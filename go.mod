f4f1c3cae5f5fd32cec7df07959fdf9ebc17f944
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-11 16:14:45 +0800

    go iris / mvc / mysql

--- go.mod
+++ go.mod
@@ -3,24 +3,28 @@ module go-note-base
 go 1.13
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/Joker/jade v1.0.0 // indirect
-	github.com/Shopify/goreferrer v0.0.0-20181106222321-ec9c9a553398 // indirect
-	github.com/aymerick/raymond v2.0.2+incompatible // indirect
-	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/flosch/pongo2 v0.0.0-20200805083417-63c99409991d // indirect
+	github.com/gavv/monotime v0.0.0-20190418164738-30dba4353424 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/iris-contrib/blackfriday v2.0.0+incompatible // indirect
 	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
-	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/iris-contrib/httpexpect v1.1.2 // indirect
+	github.com/iris-contrib/middleware/jwt v0.0.0-20200810001613-32cf668f999f
+	github.com/json-iterator/go v1.1.10
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/kataras/golog v0.1.5 // indirect
 	github.com/kataras/iris v11.1.1+incompatible
 	github.com/klauspost/compress v1.11.0 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.4 // indirect
+	github.com/onsi/ginkgo v1.14.1 // indirect
+	github.com/onsi/gomega v1.10.2 // indirect
 	github.com/ryanuber/columnize v2.1.2+incompatible // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/valyala/fasthttp v1.16.0 // indirect
+	github.com/yudai/pp v2.0.1+incompatible // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- src/go/main/IrisApplicationBootstrap.go
+++ src/go/main/IrisApplicationBootstrap.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/iris-contrib/middleware/jwt"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
 )
@@ -30,6 +35,7 @@ func main() {
 	app.RegisterView(html)
 	// 错误码
 	app.OnErrorCode(iris.StatusNotFound , notFound)
+	app.OnErrorCode(iris.StatusInternalServerError , error)
 
 	// start and set charset
 	app.Run(iris.Addr(":9090"),iris.WithCharset("UTF-8"))
@@ -44,15 +50,27 @@ func myMvc(app *mvc.Application) {
 	app.Handle(new(MyController))
 }
 
+/**
+	struct
+ */
 // controller
 type MyController struct {}
+type Token struct {
+	Token string `json:"token"`
+}
 
+/**
+	router
+
+ */
 // 再添加路由
 func (m *MyController) BeforeActivation(b mvc.BeforeActivation) {
 	// method,path,funcName,middleware
 	b.Handle("GET", "/something/{id:long}", "MyCustomHandler",hello)
 	b.Handle("GET", "/hello/{name:string}", "Hello",hello2)
 	b.Handle("GET", "/sayHello/{text:string}", "SayHelloText",hello3)
+	b.Handle("GET", "/mysqlData/{text:string}", "SayHelloText",hello3)
+	// myHandler
 }
 
 func (m *MyController) Get() string {
@@ -61,6 +79,7 @@ func (m *MyController) Get() string {
 
 /**
 	动态传参
+	params 解析
  */
 func (m *MyController) MyCustomHandler(id int64) string {
 	fmt.Println(id)
@@ -70,8 +89,21 @@ func (m *MyController) Hello(name string) string {
 	return "your name is " + name
 }
 func (m *MyController) SayHelloText(text string) string {
-	return " say :" + text
+	data := openMysql()
+	bytes, err := jsoniter.Marshal(data)
+	if err !=nil {
+		fmt.Println("err" , err.Error())
+		return "null"
+	}
+	return string(bytes)
 }
+//	data := openMysql()
+/*
+func (m *MyController) mysqlQuery() string {
+	return " jsonValue"
+}
+*/
+
 /**
 	view
  */
@@ -96,11 +128,12 @@ func hello2(ctx iris.Context) {
 }
 
 func hello3(ctx iris.Context) {
-	fmt.Println("say :")
 	ctx.Next()
 }
 
 func notFound(ctx iris.Context) {
+	// 数据填充
+	//ctx.ViewData("message","Hello world!")
 	if true {
 		err := ctx.View("hello.html")
 		if err != nil {
@@ -108,4 +141,83 @@ func notFound(ctx iris.Context) {
 		}
 	}
 }
- 
+
+func error(ctx iris.Context){
+	err := ctx.View("500.html")
+	if err != nil {
+		fmt.Println("err -> " + err.Error())
+	}
+}
+
+func myHandler( ctx iris.Context){
+	user := ctx.Values().Get("jwt").(*jwt.Token)
+	//data := openMysql()
+	ctx.JSON(user)
+}
+
+/**
+	api
+ */
+
+func openMysql() map[int]map[string]string{
+	fmt.Println("open mysql db")
+
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		fmt.Println("link error -> " + err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("err -> ping :" , err.Error())
+		return nil
+	}
+
+	// close db
+	defer db.Close()
+
+	// query
+	//rows , _ := db.Query("select * from users")
+	//id := 0
+	//name := ""
+	/*for rows.Next()  {
+	}*/
+	//fmt.Println(rows)
+
+	//query all
+	rows , _ := db.Query("select * from users;")
+	if rows == nil {
+		return nil
+	}
+	// 所有列
+	columns, _ := rows.Columns()
+	// 一行所有列的值
+	vals := make([][]byte , len(columns))
+	// 一行的填充数据
+	scans := make([]interface{}  , len(columns))
+
+	// 数据 填充
+	for k, _ := range vals {
+		scans[k] = &vals[k]
+	}
+
+	i := 0
+	result := make(map[int]map[string]string)
+		for rows.Next() {
+		//填充数据
+		rows.Scan(scans...)
+		//每行数据
+		row := make(map[string]string)
+		//把vals中的数据复制到row中
+		for k, v := range vals {
+			key := columns[k]
+			//这里把[]byte数据转成string
+			row[key] = string(v)
+		}
+		//放入结果集
+		result[i] = row
+		i++
+	}
+	return result
+}
+
