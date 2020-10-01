5a7d61ade52dc651fdc5e2e59d283f3574a05b57
qianmua <49187025+qianmua@users.noreply.github.com>
2020-10-01 22:14:10 +0800

    go test ♥

--- src/go/test/a_test.go
+++ src/go/test/a_test.go
@@ -13,3 +13,39 @@ func TestHelloWorld(t *testing.T){
 	t.Log("hello world")
 
 }
+// 多个 测试用例
+
+/**
+	一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级... 它也可以跳上 n 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+
+	动态规划
+ */
+func TestDongtaiGuiHuaA1(t *testing.T) {
+
+
+}
+
+/**
+	true
+
+	"+100"
+	"5e2"
+	"-123"
+	"3.1416"
+	"-1E-16"
+
+	false
+
+	"12e"
+	"1a3.14"
+	"1.2.3"
+	"+-5"
+	"12e+4.3"
+ */
+func TestIsNumber(t *testing.T) {
+
+}
+
+
+
+
