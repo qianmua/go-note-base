908607ad3256e23c27d9706e2ff23f517120a47f
qianmua <49187025+qianmua@users.noreply.github.com>
2020-10-01 22:05:32 +0800

    go test ♥

---
+++ src/go/test/a_test.go
@@ -0,0 +1,15 @@
+package test
+
+import "testing"
+
+/**
+ * @author
+ * @date 2020/10/1 22:04
+ * create by Goland
+ * @version 1.0
+ */ 
+ 
+func TestHelloWorld(t *testing.T){
+	t.Log("hello world")
+
+}
