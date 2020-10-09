c7964da6a79f7fb50da9992cfdcbbc98fdeb7793
qianmua <49187025+qianmua@users.noreply.github.com>
2020-10-09 16:42:33 +0800

    go test ♥

--- src/go/test/package_container_test.go
+++ src/go/test/package_container_test.go
@@ -149,4 +149,14 @@ func (s *SumInt) add (i  interface{}){
 	s.Value += i.(int)
 }
 
- 
+func TestM4(t *testing.T) {
+	r := ring.New(10)
+	for i:= 0 ; i < 10 ; i ++{
+		r.Value = i
+		r = r.Next()
+	}
+
+	sum:= SumInt{}
+	r.Do(sum.add)
+	fmt.Println(sum.Value)
+}
