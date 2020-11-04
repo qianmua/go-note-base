9e4d201a53e69d1aea9e5b9969d268047a9932ea
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-04 18:01:06 +0800

    go note / god way ♥

--- src/go/go_data_plan/go_xml/Go_Data_Xml_test.go
+++ src/go/go_data_plan/go_xml/Go_Data_Xml_test.go
@@ -26,13 +26,14 @@ import (
 
 
 // 基础结构体
+// 基础标签
 type RecServices struct {
 	XMLName xml.Name `xml:"servers"`
 	Version string `xml:"version,attr"`
-	Svs	[]server `xml:"svs"`
+	Svs	[]server `xml:"server"`
 	Description string `xml:",innerxml"`
 }
-
+// value
 type server struct {
 	XMLName    xml.Name `xml:"server"`
 	ServerName string   `xml:"serverName"`
