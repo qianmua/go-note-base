88c599aae592782bac7d6f52f7f888e1b958930c
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-04 15:07:36 +0800

    go note / god way ♥

--- src/go/go_web_http/Go_Web_session_test.go
+++ src/go/go_web_http/Go_Web_session_test.go
@@ -1,8 +1,15 @@
 package test
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"github.com/astaxie/beego/session"
+	"net/http"
+	"net/url"
 	"sync"
+	"testing"
+	"time"
 )
 
 /**
@@ -53,6 +60,39 @@ func Register(name string , provider Provider){
 	}
 	provides[name] = provider
 }
+// gen session id
+// 全局唯一的（GUID）
+func (manager *Manager) sessionId()string {
+	b := make([]byte , 32)
+	if _ ,err := rand.Read(b) ; err != nil{
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+// 检测是否已经有某个Session与当前来访用户发生了关联，如果没有则创建之
+func (manager *Manager) SessionStart(w http.ResponseWriter , r *http.Request) (session Session){
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		sessionId := manager.sessionId()
+		session, _ = manager.provider.SessionInit(sessionId)
+		cookie := http.Cookie{
+			Name:     manager.cookieName,
+			Value:    url.QueryEscape(sessionId),
+			Path:     "/",
+			Expires:  time.Time{},
+			MaxAge:   int(manager.maxLifeTime),
+			HttpOnly: true,
+		}
+		http.SetCookie(w , &cookie)
+	}else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
 
 /**
 创建 session
@@ -63,4 +103,21 @@ func NewManager(provideName, cookieName string, maxLifeTime int64) (*Manager, er
 		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
 	}
 	return &Manager{provider: provider, cookieName: cookieName, maxLifeTime: maxLifeTime}, nil
+}
+
+
+// global management
+var globalSessions *Manager
+//然后在init函数中初始化
+func init() {
+	globalSessions, _ = NewManager("memory", "gosessionid", 3600)
+}
+
+// test login
+func TestLogin(t *testing.T) {
+
+}
+
+func login2(w http.ResponseWriter , r *http.Request){
+	SessionStart
 }
