d89652018367ca96388ce2dd673c6958dec38f4f
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-05 16:51:13 +0800

    go note / god way ♥

---
+++ src/go/go_data_plan/go_string/Go_Data_String_test.go
@@ -0,0 +1,43 @@
+package go_string
+
+/**
+ * @author
+ * @date 2020/11/5 16:34
+ * create by Goland
+ * @version 1.0
+ */
+
+// string 常用操作api
+
+// Contains
+
+// Join
+// 把slice a通过sep链接起来
+
+// Index
+// 在字符串s中查找sep所在的位置，返回位置值，找不到返回-1
+
+// Repeat
+// 重复s字符串count次，最后返回重复的字符串
+
+
+// Replace
+// 在s字符串中，把old字符串替换为new字符串，n表示替换的次数，小于0表示全部替换
+
+// Split
+
+// Trim
+// 在s字符串的头部和尾部去除cutset指定的字符串
+
+// Fields
+// 去除s字符串的空格符，并且按照空格分割返回slice
+
+
+// 字符串转换
+// strconv
+
+// Append 系列函数将整数等转换为字符串后，添加到现有的字节数组中。
+
+// Format 系列函数把其他类型的转换为字符串
+
+// Parse 系列函数把字符串转换为其他类型
