090f6f17f6c868766be4ce1e5cd4810a98f89a20
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-10 16:39:34 +0800

    go note / god way ♥

--- src/go/go_safe/go_crypto/Go_Safe_Aes_test.go
+++ src/go/go_safe/go_crypto/Go_Safe_Aes_test.go
@@ -1,6 +1,12 @@
 package go_crypto
 
-import "testing"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"os"
+	"testing"
+)
 
 /**
  * @author
@@ -14,5 +20,43 @@ import "testing"
 var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
 
 func TestAESM1(t *testing.T) {
+
+	name := []byte("name is qianmuna")
+	//if len(os.Args) > 1 {
+	//	name = []byte(os.Args[1])
+	//}
+
+	//aes的加密字符串
+	// 16 24 32
+	// aes128 aes192 aes256
+	key_text := "qianmua12798akljzmknm.ahkjkljl;k"
+	/*if len(os.Args) > 2 {
+		key_text = os.Args[2]
+	}*/
+
+	fmt.Println(len(key_text))
+
+	// create aes
+	c, err := aes.NewCipher([]byte(key_text))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(key_text), err)
+		os.Exit(-1)
+	}
+
+
+	// encode
+	cfbe := cipher.NewCFBEncrypter(c, commonIV)
+	var1 := make([]byte, len(name))
+	cfbe.XORKeyStream(var1 , name)
+	// var1 加密后
+	fmt.Printf("%s=>%x\n", name, var1)
+
+	//decode
+	cfbd := cipher.NewCFBDecrypter(c, commonIV)
+	var2 := make([]byte, len(name))
+	//var var3 []byte
+	cfbd.XORKeyStream(var2 , var1)
+
+	fmt.Printf("%x=>%s\n", var1, var2)
 
 }
