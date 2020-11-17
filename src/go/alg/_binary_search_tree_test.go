ae40bc8f59471188b16461d158d9c28bd5fe1690
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-17 23:43:10 +0800

    go note / god way ♥

--- src/go/alg/_binary_search_tree_test.go
+++ src/go/alg/_binary_search_tree_test.go
@@ -34,5 +34,41 @@ func insertIntoBST(root *TreeNode , val int)*TreeNode{
 
 // 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的  key  对应的节点，并保证二叉搜索树的性质不变。
 //返回二叉搜索树（有可能被更新）的根节点的引用。
+func deleteNode(root *TreeNode , key int)*TreeNode{
 
+	// 删除节点分为三种情况：
+	// 1、只有左节点 替换为右
+	// 2、只有右节点 替换为左
+	// 3、有左右子节点 左子节点连接到右边最左节点即可
+
+	if root == null {
+		return root
+	}
+
+	if root.Val < key {
+		//search
+		root.Right = deleteNode(root.Right , key)
+	}else if root.Val > key{
+		// search
+		root.Left = deleteNode(root.Left , key)
+	}else if root.Val == key{
+		if root.Left == nil {
+			return root.Right
+		}else if root.Right == nil {
+			return root.Left
+		}else {
+			cur := root.Right
+			for cur.Left != nil  {
+				cur = cur.Left
+			}
+
+			cur.Left = root.Left
+
+			return root.Right
+		}
+	}
+
+	return root
+
+}
 
---
+++ src/go/alg/_recursion_test.go
@@ -0,0 +1,20 @@
+package alg
+
+/**
+ * @author
+ * @date 2020/11/17 23:42
+ * create by Goland
+ * @version 1.0
+ */
+
+
+// 递归
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组  `char[]`  的形式给出。
+func reverseString(s []byte){
+	res := make([]byte , 0)
+
+
+
+
+}
+ 
