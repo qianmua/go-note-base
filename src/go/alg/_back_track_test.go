ac6cffe9b03caf5a8d590159ac1c0c99b9bd6479
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-17 23:24:17 +0800

    go note / god way ♥

--- src/go/alg/_back_track_test.go
+++ src/go/alg/_back_track_test.go
@@ -8,4 +8,74 @@ package alg
  */
 
 // 回溯
- 
+
+
+// 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+
+func subSets(nums []int)[][]int{
+	res := make([][]int , 0)
+
+	list := make([]int ,0)
+
+	backtrack(nums , 0 , list , &res)
+
+	return res
+}
+
+// pos 下次添加到集合中的元素位置索引
+// list 临时结果集合(每次需要复制保存)
+func backtrack(nums []int , pos int , list []int , res *[][]int){
+	ans := make([]int , len(list))
+
+	copy(ans , list)
+
+	*res = append(*res , ans)
+
+	for i := pos; i <len(nums) ;i ++  {
+		list = append(list, nums[i])
+		backtrack(nums , i + 1 , list , res)
+
+		list = list[0 : len(list) - 1]
+		
+	}
+}
+
+
+
+/// 全排列
+func permute(nums []int) [][]int {
+
+	result := make([][]int, 0)
+	list := make([]int, 0)
+
+	// 标记这个元素是否已经添加到结果集
+	visited := make([]bool, len(nums))
+	backtrack2(nums, visited, list, &result)
+
+	return result
+}
+
+func backtrack2(nums []int, visited []bool, list []int, result *[][]int) {
+	// 返回条件：临时结果和输入集合长度一致 才是全排列
+	if len(list) == len(nums) {
+		ans := make([]int, len(list))
+		copy(ans, list)
+		*result = append(*result, ans)
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		// 已经添加过的元素，直接跳过
+		if visited[i] {
+			continue
+		}
+		// 添加元素
+		list = append(list, nums[i])
+		visited[i] = true
+		backtrack(nums, visited, list, result)
+		// 移除元素
+		visited[i] = false
+		list = list[0 : len(list)-1]
+	}
+}
+
+
