4e45eeacac91f7fa648cbc16c39af14616500f5b
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-19 17:13:40 +0800

    go note / god way ♥

--- src/go/alg_leetcode/Go_14_longest_common_prefix_test.go
+++ src/go/alg_leetcode/Go_14_longest_common_prefix_test.go
@@ -8,4 +8,23 @@ package alg_leetcode
  */ 
  
 // 最长相同前缀
-//
+func longPrefix(strs []string)string{
+	if strs == nil || len(strs) == 0 {
+		return ""
+	}
+
+	// each
+	for i:= 0 ; i < len(strs[0]) ; i ++{
+		var c byte = strs[0][i]
+		// 比较后面
+
+		for j := 1 ; j < len(strs) ; j ++{
+
+			if i == len(strs[j]) || strs[j][i] != c {
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0]
+}
